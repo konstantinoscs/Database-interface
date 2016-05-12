fbe0d88710a2fd5bfa60f6d29b5e096cf9ced1a6
konstantinos <kons.ste@gmail.com>
2016-05-12 13:47:57 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,248 @@
+
+from bottle import get, post, request, run, route
+
+@route('/')
+@route('/home')
+def home_page():
+    return '''
+    <div>
+
+    <form action="/update_n_search">
+    <input type="submit" value="UPDATE & SEARCH ARTISTS">
+    </form>
+
+    <form action="/search_songs">
+    <input type="submit" value="SEARCH SONGS">
+    </form>
+
+    <form action="/insert_artist">
+    <input type="submit" value="INSERT ARTIST">
+    </form>
+    <form action="/insert_song">
+    <input type="submit" value="INSERT SONG">
+    </form>
+    </div>
+'''
+
+@get('/update_n_search')
+def update():
+    return '''
+        <div>
+        <div style="font-size:200%;"><strong>Presentation of Artists</strong></div>
+		<br>
+
+        <form>
+        <fieldset>
+			<table style="">
+				<tr>
+					<td> <span align="left">Name </span></td>
+					<td> <input align="right" type="text" name="Name" value=""></td>
+				</tr>
+
+				<tr>
+					<td> <span align="left"> Surname </span></td>
+					<td> <input align="right" type="text" name="Surname" value=""> </td>
+				</tr>
+
+				<tr>
+        			<td>Birth Year - From</td>
+					<td> <input type="text" name="Birth_year_from" value=""> </td>
+				</tr>
+
+				<tr>
+					<td> Birth Year - To </td>
+					<td> <input type="text" name="Birth_year_to" value=""> </td>
+				</tr>
+
+				<tr>
+					<td>Type</td>
+					<td>
+						<input type="radio" name="type" value="Singer"> Singer <br>
+        				<input type="radio" name="type" value="SongWriter"> SongWriter <br>
+        				<input type="radio" name="type" value="Composer"> Composer <br>
+					</td>
+				</tr>
+				<tr>
+					<td></td>
+					<td><input type="submit" value="Submit"></td>
+				</tr>
+			</table>
+        </fieldset>
+        </form>
+        </div>
+        '''
+
+@post('/update_n_search')
+def get_data_update():
+    name=request.forms.get('Name')
+    surname=request.forms.get('Surname')
+    birth_year_from=request.forms.get('Birth_year_from')
+    birth_year_to=request.forms.get('Birth_year_to')
+    type=request.forms.get('type')
+
+
+
+@get('/search_songs')
+def search_songs():
+    return '''
+        <div>
+            <div style="font-size:200%;"><strong>Presentation of Songs</strong></div>
+            <br>
+
+            <form>
+                <fieldset>
+                    <table style="">
+                        <tr>
+                            <td> <span align="left">Song Title </span></td>
+                            <td> <input align="right" type="text" name="Song_title" value=""> </td>
+                        </tr>
+
+
+                        <tr>
+                            <td> <span align="left">Production Year</span></td>
+                            <td> <input align="right" type="text" name="Prod_year" value=""> </td>
+                        </tr>
+
+
+                        <tr>
+                            <td> <span align="letf">Company</span></td>
+                            <td> <input align="right" type="text" name="Company" value=""> </td>
+                        </tr>
+
+
+                        <tr>
+                            <td></td>
+                            <td> <input type="submit" value="Submit"> </td>
+                        </tr>
+
+
+                    </table>
+                </fieldset>
+            </form>
+    </div>
+'''
+
+@post('/search_songs')
+def get_search_songs():
+    song_title=request.forms.get('Song_title')
+    prod_year=request.forms.get('Prod_year')
+    company=request.forms.get('Company')
+
+@get('/insert_artist')
+def insert_artist():
+    return '''
+    <div>
+        <div style="font-size:200%;"><strong>Insert Artist</strong></div>
+        <br>
+
+        <form>
+            <fieldset>
+                <table style="">
+                    <tr>
+                        <td> <span align="left">National Id </span></td>
+                        <td> <input align="right" type="text" name="National_id" value=""> </td>
+                    </tr>
+
+
+                    <tr>
+                        <td> <span align="left">Name</span></td>
+                        <td> <input align="right" type="text" name="Name" value=""> </td>
+                    </tr>
+
+
+                    <tr>
+                        <td> <span align="letf">Surname</span></td>
+                        <td> <input align="right" type="text" name="Surname" value=""> </td>
+                    </tr>
+
+
+                    <tr>
+                        <td> <span align="left">Birth Year</span></td>
+                        <td> <input align="right" type="number" name="Birth_year" min="1900" max="2016"> </td>
+                    </tr>
+
+                    <tr>
+                        <td></td>
+                        <td> <input type="submit" value="Update Information"> </td>
+                    </tr>
+
+
+                </table>
+            </fieldset>
+        </form>
+    </div>
+'''
+
+@post('/insert_artist')
+def get_insert_artist():
+    national_id=request.forms.get('National_id')
+    name=request.forms.get('Name')
+    surname=request.forms.get('Surname')
+    birth_year=request.forms.get('Birth_year')
+
+
+@get('/insert_song')
+def insert_song():
+    return '''
+    <div>
+        <div style="font-size:200%;"><strong>Insert Song</strong></div>
+        <br>
+
+        <form>
+            <fieldset>
+                <table style="">
+                    <tr>
+                        <td> <span align="left">Title</span></td>
+                        <td> <input align="right" type="text" name="Title" value=""> </td>
+                    </tr>
+
+
+                    <tr>
+                        <td> <span align="left">Production Year</span></td>
+                        <td> <input align="right" type="text" name="Prod_year" value=""> </td>
+                    </tr>
+
+
+                    <tr>
+                        <td> <span align="letf">CD</span></td>
+                        <td> <input align="right" type="text" name="Surname" value=""> </td>
+                    </tr>
+
+
+                    <tr>
+                        <td> <span align="left">Singer</span></td>
+                        <td> <input align="right" type="number" name="Birth_year" min="1900" max="2016"> </td>
+                    </tr>
+
+
+                    <tr>
+                        <td> <span align="left">Composer</span></td>
+                        <td> <input align="right" type="number" name="Birth_year" min="1900" max="2016"> </td>
+                    </tr>
+
+
+                    <tr>
+                        <td> <span align="left">Song Writer</span></td>
+                        <td> <input align="right" type="number" name="Birth_year" min="1900" max="2016"> </td>
+                    </tr>
+
+
+                    <tr>
+                        <td></td>
+                        <td> <input type="submit" value="Submit"> </td>
+                    </tr>
+
+
+                </table>
+            </fieldset>
+        </form>
+    </div>
+'''
+
+@post('/insert_song')
+def get_insert_songs():
+    title=request.forms.get('Title')
+    prod_year=request.forms.get('Prod_year')
+
+
+run(host='localhost', port=8080, debug=True)
