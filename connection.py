0e2d586ea6bb95ceb347024ba11ef334afa71742
konstantinos <kons.ste@gmail.com>
2016-05-14 17:31:27 +0300

    connection

--- connection.py
+++ connection.py
@@ -1,3 +1,10 @@
 import pymysql
 import settings
 
+def connection():
+    con = pymysql.connect(
+        settings.mysql_host,
+        settings.mysql_user,
+        settings.mysql_passwd,
+        settings.mysql_schema)
+    return con
